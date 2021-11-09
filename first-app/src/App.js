1396f2452076245fd11356249cf508f07f9bd345
letx00x <xeltrux@gmail.com>
2021-11-09 23:57:38 +0300

    vid7

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,24 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (   
+    <div> 
+      <Header />
+      <Technologies />
+    </div>   
   );
+}
+
+function Header() {
+  return(
+    <h1>Header</h1>
+  )
+}
+
+function Technologies(){
+  return(
+    <div>
+        <ul>
+          <li>Node</li>
+          <li>React</li>
+          <li>Angular</li>
+          <li>HTML</li>
+        </ul>
+      </div>
+  )
 }
 
 export default App;
