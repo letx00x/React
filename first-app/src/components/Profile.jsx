00edc43845f3b5e82839b7dd031347824d5357b1
letx00x <xeltrux@gmail.com>
2021-11-11 22:53:10 +0300

    13-added componets folder

---
+++ first-app/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+const Profile = () =>{
+  return (
+    <div className = 'content'>
+        <img src = "https://cloud.pulse19.ru/uploads/2020/05/dhxh3box4aa1qyo.jpg"/>
+        <div>ava+description</div>
+        <div>My posts</div>
+        <div>New Post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+  ) 
+}
+
+export default Profile
