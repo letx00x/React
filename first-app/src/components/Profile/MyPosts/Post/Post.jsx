8ec51c36bc1ddf66142234c9a9a9c2b0f0b2a4b2
letx00x <xeltrux@gmail.com>
2021-11-17 23:53:57 +0300

    19.Add Dialogs

--- first-app/src/components/Profile/MyPosts/Post/Post.jsx
+++ first-app/src/components/Profile/MyPosts/Post/Post.jsx
@@ -6,7 +6,7 @@ const Post = (props) =>{
              <img src = 'https://miro.medium.com/fit/c/256/256/1*CKZ5ixjxTF3vuzsXV1Utkw.png'></img>  
              {props.message}   
              <div>
-              <span>Like {props.likes}</span>  
+              <span>Like</span> {props.likes}
              </div>       
           </div> 
   ) 
--- first-app/src/components/Profile/Profile.jsx
+++ first-app/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import classes from './Profile.module.css';
 
 const Profile = () =>{
   return (
-    <div className = {classes.content}>
+    <div>
         <img src = "https://cloud.pulse19.ru/uploads/2020/05/dhxh3box4aa1qyo.jpg"/>
         <div>ava+description</div>
         
