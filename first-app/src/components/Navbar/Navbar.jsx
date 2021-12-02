7a7a88b43ae456da530de2d1a08b9b78cf637bba
letx00x <letx00x@gmail.com>
2021-12-02 20:59:13 +0300

    20.Navlink

--- first-app/src/components/Navbar/Navbar.jsx
+++ first-app/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () =>{
   return (
     <nav className = {classes.nav}>
         <div className = {classes.item}>
-        <a href = '/profile'>Profile</a>
+        <NavLink className = {navData => navData.isActive ? classes.active : classes.item} to = '/profile'>Profile</NavLink>
         </div>
         <div className = {`${classes.item} ${classes.active}`}>
-          <a href = '/dialogs'>Messages</a>
+        <NavLink className = {navData => navData.isActive ? classes.active : classes.item} to = '/dialogs'>Messages</NavLink>
           </div>
         <div className = {classes.item}>
-          <a>News</a>
+          <NavLink to = '/news' className = {navData => navData.isActive ? classes.active : classes.item}>News</NavLink>
           </div>
         <div className = {classes.item}>
-          <a>Music</a>
+          <NavLink to = '/music' className = {navData => navData.isActive ? classes.active : classes.item}>Music</NavLink>
           </div>
         <div className = {classes.item}>
-          <a>Settings</a>
+          <NavLink to = '/settings' className = {navData => navData.isActive ? classes.active : classes.item}>Settings</NavLink>
           </div>
       </nav>
   )
