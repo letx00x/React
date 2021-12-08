4feb7b074a8ca68f4738896da0b2621b84b1fb08
letx00x <letx00x@gmail.com>
2021-12-08 22:50:55 +0300

    23.PropsForDialogs

--- first-app/src/components/Dialogs/Dialogs.jsx
+++ first-app/src/components/Dialogs/Dialogs.jsx
@@ -1,29 +1,37 @@
 import classes from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
+
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+
+    return (
+        <div className = {classes.dialog + ' ' + classes.active}>
+            <NavLink to = {path}>{props.name}</NavLink> 
+        </div>
+    )
+}
+
+const MessageItem = (props) =>{
+    return(
+        <div className = {classes.message}>{props.message}</div>
+    )
+}
 
 const Dialogs = (props) => {
     return (
         <div className = {classes.dialogs}>
             <div className = {classes.dialogsItems}>
-                <div className = {classes.dialog + ' ' + classes.active}>
-                    Alex
-                </div>
-                <div className = {classes.dialog}>
-                    Tony
-                </div>
-                <div className = {classes.dialog}>
-                    Boris
-                </div>
-                <div className = {classes.dialog}>
-                    Max
-                </div>
-                <div className = {classes.dialog}>
-                    John
-                </div>
+                <DialogItem name = 'Alex' id = "1"/>
+                <DialogItem name = "Max" id = "2" />
+                <DialogItem name = "Jein" id = "3" />
+                <DialogItem name = "Julia" id = "4" />
+                <DialogItem name = "Anna" id = "5" />
+                <DialogItem name = "Lili" id = "6" />
             </div>
             <div className = {classes.messages}>
-                <div className = {classes.message}>Hi</div>
-                <div className = {classes.message}>How are you</div>
-                <div className = {classes.message}>Bye</div>
+                <MessageItem message = "Hi"/>
+                <MessageItem message = "How are you"/>
+                <MessageItem message = "Bye"/>
             </div>
         </div>
     )
--- first-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ first-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,14 @@ import Post from './Post/Post';
 
 const MyPosts = () =>{
   return (
-        <div>My posts
+        <div className = {classes.postsBlock}><h3>My posts</h3>
           <div>
-            <textarea></textarea>
+            <div>
+              <textarea></textarea>
+            </div>  
+            <div>
             <button>add</button>
+            </div>
           </div>        
         <div className = {classes.posts}>
           <Post message = 'Hello, i am Alex' likes = '4' />     
